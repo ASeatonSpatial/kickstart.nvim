473d315a62a2e7822c1e07ad4b40fed2c15dc101
Andy Seaton <Andy.e.seaton@gmail.com>
2025-02-07 14:49:35 +0000

    add quarto LSP things

--- init.lua
+++ init.lua
@@ -643,6 +643,11 @@ require('lazy').setup({
             diagnostics = false,
           },
         },
+
+        -- Markdown/quarto support:
+        marksman = {},
+        yamlls = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
