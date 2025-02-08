5f8bdec02bbe2615094b2a449800b3a5753262d7
Andy Seaton <Andy.e.seaton@gmail.com>
2025-02-08 03:25:09 +0000

    add mini.icons for which-key

--- init.lua
+++ init.lua
@@ -287,42 +287,42 @@ require('lazy').setup({
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
       delay = 0,
-      icons = {
-        -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
-        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-        keys = vim.g.have_nerd_font and {} or {
-          Up = '<Up> ',
-          Down = '<Down> ',
-          Left = '<Left> ',
-          Right = '<Right> ',
-          C = '<C-…> ',
-          M = '<M-…> ',
-          D = '<D-…> ',
-          S = '<S-…> ',
-          CR = '<CR> ',
-          Esc = '<Esc> ',
-          ScrollWheelDown = '<ScrollWheelDown> ',
-          ScrollWheelUp = '<ScrollWheelUp> ',
-          NL = '<NL> ',
-          BS = '<BS> ',
-          Space = '<Space> ',
-          Tab = '<Tab> ',
-          F1 = '<F1>',
-          F2 = '<F2>',
-          F3 = '<F3>',
-          F4 = '<F4>',
-          F5 = '<F5>',
-          F6 = '<F6>',
-          F7 = '<F7>',
-          F8 = '<F8>',
-          F9 = '<F9>',
-          F10 = '<F10>',
-          F11 = '<F11>',
-          F12 = '<F12>',
-        },
-      },
+      -- icons = {
+      --   -- set icon mappings to true if you have a Nerd Font
+      --   mappings = vim.g.have_nerd_font,
+      --   -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+      --   -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+      --   keys = vim.g.have_nerd_font and {} or {
+      --     Up = '<Up> ',
+      --     Down = '<Down> ',
+      --     Left = '<Left> ',
+      --     Right = '<Right> ',
+      --     C = '<C-…> ',
+      --     M = '<M-…> ',
+      --     D = '<D-…> ',
+      --     S = '<S-…> ',
+      --     CR = '<CR> ',
+      --     Esc = '<Esc> ',
+      --     ScrollWheelDown = '<ScrollWheelDown> ',
+      --     ScrollWheelUp = '<ScrollWheelUp> ',
+      --     NL = '<NL> ',
+      --     BS = '<BS> ',
+      --     Space = '<Space> ',
+      --     Tab = '<Tab> ',
+      --     F1 = '<F1>',
+      --     F2 = '<F2>',
+      --     F3 = '<F3>',
+      --     F4 = '<F4>',
+      --     F5 = '<F5>',
+      --     F6 = '<F6>',
+      --     F7 = '<F7>',
+      --     F8 = '<F8>',
+      --     F9 = '<F9>',
+      --     F10 = '<F10>',
+      --     F11 = '<F11>',
+      --     F12 = '<F12>',
+      --   },
+      -- },
 
       -- Document existing key chains
       spec = {
@@ -875,8 +875,8 @@ require('lazy').setup({
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
-          { name = 'nvim_lsp' },
           { name = 'otter' },
+          { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
         },
@@ -925,6 +925,9 @@ require('lazy').setup({
 
       -- Comment out lines
       require('mini.comment').setup()
+
+      -- Icons required for which-key
+      require('mini.icons').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
