0af485586876cefdf68eec7c522e4213d2b94b8d
Andy Seaton <Andy.e.seaton@gmail.com>
2025-02-10 07:41:57 +0000

    add alpha landing page

--- init.lua
+++ init.lua
@@ -265,6 +265,20 @@ require('lazy').setup({
       },
     },
   },
+  { -- Nice landing page
+    "goolord/alpha-nvim",
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(
+        startify.config
+      )
+    end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
