5a97ad345fadc2c50c57d43a3f495f34a5f87cfe
Andy Seaton <Andy.e.seaton@gmail.com>
2025-02-10 06:21:41 +0000

    gx plugin not working as described?

--- init.lua
+++ init.lua
@@ -479,6 +479,26 @@ require('lazy').setup({
     },
   },
   {
+   'nvim-lua/plenary.nvim'
+  },
+  { -- better gx command for opening urls
+   'chrishrb/gx.nvim',
+    enabled = false,
+    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
+    cmd = { 'Browse' },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    submodules = false, -- not needed, submodules are required only for tests
+    opts = {
+      handler_options = {
+        -- you can select between google, bing, duckduckgo, and ecosia
+        search_engine = 'google',
+      },
+    },
+  },
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
