ad4bd72a440f7fd5b23d3bb4ac87a4a93779c420
Andy Seaton <Andy.e.seaton@gmail.com>
2025-02-11 16:44:13 +0000

    add treesitter playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,37 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dev = false,
     dependencies = {
       {
-        'nvim-treesitter/nvim-treesitter-textobjects',
+        "nvim-treesitter/nvim-treesitter-textobjects",
       },
     },
-    run = ':TSUpdate',
+    run = ":TSUpdate",
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
+      require("nvim-treesitter.configs").setup({
         auto_install = true,
         ensure_installed = {
-          'r',
-          'python',
-          'markdown',
-          'markdown_inline',
-          'julia',
-          'bash',
-          'yaml',
-          'lua',
-          'vim',
-          'query',
-          'vimdoc',
-          'latex', -- requires tree-sitter-cli (installed automatically via Mason)
-          'html',
-          'css',
-          'dot',
-          'javascript',
-          'mermaid',
-          'norg',
-          'typescript',
+          "r",
+          "python",
+          "markdown",
+          "markdown_inline",
+          "julia",
+          "bash",
+          "yaml",
+          "lua",
+          "vim",
+          "query",
+          "vimdoc",
+          "latex", -- requires tree-sitter-cli (installed automatically via Mason)
+          "html",
+          "css",
+          "dot",
+          "javascript",
+          "mermaid",
+          "norg",
+          "typescript",
         },
         highlight = {
           enable = true,
@@ -43,10 +43,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
           },
         },
         textobjects = {
@@ -55,34 +55,59 @@ return {
             lookahead = true,
             keymaps = {
               -- You can use the capture groups defined in textobjects.scm
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
             },
           },
           move = {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.inner',
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.inner",
             },
             goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
             },
             goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.inner',
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.inner",
             },
             goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
             },
           },
         },
-      }
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/playground",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = "o",
+            toggle_hl_groups = "i",
+            toggle_injected_languages = "t",
+            toggle_anonymous_nodes = "a",
+            toggle_language_display = "I",
+            focus_language = "f",
+            unfocus_language = "F",
+            update = "R",
+            goto_node = "<cr>",
+            show_help = "?",
+          },
+        },
+      })
     end,
   },
 }
